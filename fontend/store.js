2201b237781ab4847e2e7edb3d7cf9bd758f8251
peogway <duchungbeo2004@gmail.com>
2025-01-12 10:07:45 +0200

    configuration for storing user and notification

---
+++ fontend/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import notiReducer from "./reducers/notiReducer";
+import userReducer from "./reducers/userReducer";
+
+export default configureStore({
+    reducer: {
+        notiReducer,
+        user: userReducer,
+    },
+});
