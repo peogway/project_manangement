3ef598a637fef1406e34449e62c202e768d7f541
peogway <duchungbeo2004@gmail.com>
2025-01-13 14:59:32 +0200

    route for searching all  prjs

--- backend/controllers/projects.js
+++ backend/controllers/projects.js
@@ -1,2 +1,22 @@
 const User = require("../models/user");
 const Task = require("../models/task");
+const Project = require("../models/project");
+const Category = require("../models/category");
+
+const projectsRouter = require("express").Router();
+
+projectsRouter.get("/", async (req, res) => {
+    const projects = await Project.find({}).populate("tasks", {
+        id: 1,
+        name: 1,
+    })
+        .populate("categories", { id: 1, name: 1 }).populate("user", {
+            id: 1,
+            name: 1,
+            useranme: 1,
+        });
+
+    res.status(200).json(projects);
+});
+
+module.exports = projectsRouter;
