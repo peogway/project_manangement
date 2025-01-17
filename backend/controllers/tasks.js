906f21ffd1c697d1cb0d83dda78fe1a1cdd2ccd0
peogway <duchungbeo2004@gmail.com>
2025-01-17 13:35:46 +0200

    route for adding task

--- backend/controllers/tasks.js
+++ backend/controllers/tasks.js
@@ -23,4 +23,36 @@ tasksRouter.get("/", async (req, res) => {
     res.status(200).json(tasks);
 });
 
+tasksRouter.post("/", async (req, res) => {
+    const userRequest = req.user;
+    if (!userRequest) return res.status(401).json({ error: "invalid token" });
+
+    const { projectId } = req.query;
+    const body = req.body;
+
+    const project = await Project.findById(projectId);
+    if (!project) return res.status(400).json({ error: "invalid project id" });
+
+    if (project.user !== userRequest.id.toString()) {
+        return res.status(403).json({
+            error: "only project owner can add task",
+        });
+    }
+
+    const task = new Task({
+        ...body,
+        status: "in-progress",
+        completed: false,
+        createAt: new Date(),
+        project: project.id,
+    });
+
+    const savedTask = await task.save();
+    const populatedTask = await savedTask.populate(
+        "project",
+        "id name categories",
+    );
+    res.status(201).json(populatedTask);
+});
+
 module.exports = tasksRouter;
