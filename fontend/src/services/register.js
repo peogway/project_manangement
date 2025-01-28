8e3ca187cc143e97367b682a5b93a8f636733690
peogway <duchungbeo2004@gmail.com>
2025-01-28 15:00:39 +0200

    delete folder fontend

--- fontend/src/services/register.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-const baseUrl = "http://localhost:3001/api/users";
-
-const register = async (credentials) => {
-    const response = await axios.post(baseUrl, credentials);
-    return response.data;
-};
-
-export default { register };
