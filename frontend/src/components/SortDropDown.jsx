781364e6f91df30a66781441115a603ce730189d
peogway <duchungbeo2004@gmail.com>
2025-02-01 21:30:28 +0200

    sort dropdown component

---
+++ frontend/src/components/SortDropDown.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+
+const SortDropdown = ({ setSortValue }) => {
+	const [selectedSort, setSelectedSort] = useState('A-Z')
+
+	const handleChange = (e) => {
+		setSelectedSort(e.target.value)
+		setSortValue(e.target.value) // Pass value up
+	}
+	return (
+		<select value={selectedSort} onChange={handleChange}>
+			<optgroup label='Order'>
+				<option value='A-Z'>A-Z</option>
+				<option value='Z-A'>Z-A</option>
+			</optgroup>
+			<optgroup label='Date'>
+				<option value='newest'>Newest</option>
+				<option value='oldest'>Oldest</option>
+			</optgroup>
+		</select>
+	)
+}
+
+export default SortDropdown
+
