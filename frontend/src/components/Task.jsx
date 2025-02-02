26e728a47f45392145731b0f0adfc4b2cfa3b6dd
peogway <duchungbeo2004@gmail.com>
2025-02-02 22:59:51 +0200

    pass projects to EditForm component

--- frontend/src/components/Task.jsx
+++ frontend/src/components/Task.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import { updateTask } from '../reducers/taskReducer'
 import { useDispatch } from 'react-redux'
+import EditTaskForm from './EditTaskForm'
 
 const Task = ({ ...props }) => {
 	const dispatch = useDispatch()
 	const [isChecked, setIsChecked] = useState(props.completed ? true : false)
+	const [showEditForm, setShowEditForm] = useState(false)
 	const handleCheckboxChange = () => {
 		setIsChecked(!isChecked)
 		const { id } = props
@@ -17,39 +19,44 @@ const Task = ({ ...props }) => {
 		dispatch(updateTask(taskToUpdate))
 	}
 	return (
-		<div className='task-container'>
-			<div className='left-content'>
-				<input
-					className='task-checkbox'
-					type='checkbox'
-					checked={isChecked}
-					onChange={handleCheckboxChange}
-				/>
-			</div>
-			<div className='right-content'>
-				<div className='task-name-project'>
-					<h2 className='task-name'>{props.name}</h2>
-					<p className='task-project'>{props.project.name}</p>
+		<div>
+			<div className='task-container'>
+				<div className='left-content'>
+					<input
+						className='task-checkbox'
+						type='checkbox'
+						checked={isChecked}
+						onChange={handleCheckboxChange}
+					/>
 				</div>
-				<div className='status'>
-					{props.completed ? <div>Completed</div> : <div>In Progress</div>}
+				<div className='right-content'>
+					<div className='task-name-project'>
+						<h2 className='task-name'>{props.name}</h2>
+						<p className='task-project'>{props.project.name}</p>
+					</div>
+					<div className='status'>
+						{props.completed ? <div>Completed</div> : <div>In Progress</div>}
+					</div>
+
+					<div className='priority'>{props.priority}</div>
+
+					<button
+						onClick={() => setShowEditForm(true)}
+						className='edit-task-btn'
+					>
+						Edit
+					</button>
+					<button
+						onClick={() => props.deleteTask(props.id, props.project.id)}
+						className='delete-task-btn'
+					>
+						Delete
+					</button>
 				</div>
-
-				<div className='priority'>{props.priority}</div>
-
-				<button
-					onClick={() => props.editTask(props.id, props.project.id)}
-					className='edit-task-btn'
-				>
-					Edit
-				</button>
-				<button
-					onClick={() => props.deleteTask(props.id, props.project.id)}
-					className='delete-task-btn'
-				>
-					Delete
-				</button>
 			</div>
+			{showEditForm && (
+				<EditTaskForm onClose={() => setShowEditForm(false)} {...props} />
+			)}
 		</div>
 	)
 }
