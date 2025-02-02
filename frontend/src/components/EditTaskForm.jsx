5bf4117011f9f063c1af02e0565efa7b1e825d3d
peogway <duchungbeo2004@gmail.com>
2025-02-02 23:00:40 +0200

    EditTaskForm component can edit task

--- frontend/src/components/EditTaskForm.jsx
+++ frontend/src/components/EditTaskForm.jsx
@@ -1,7 +1,81 @@
-import { useState } from 'react'
+import { useField } from '../hooks/hook'
+import { useRef, useEffect, useState } from 'react'
+import Dropdown from './DropDown'
+import { setError, setNotification } from '../reducers/notiReducer'
+import { updateTask } from '../reducers/taskReducer'
+import { useDispatch } from 'react-redux'
 
-const EditTaskForm = (props) => {
-	return <div></div>
+const EditTaskForm = ({ onClose, projects, selectedProject, ...task }) => {
+	const { remove: rmTask, ...taskName } = useField('text', task.name)
+	const [priority, setPriority] = useState(task.priority)
+	const [chosenProject, setChosenProject] = useState(task.project.name)
+	const formRef = useRef(null)
+	const dispatch = useDispatch()
+	useEffect(() => {
+		const handleClickOutside = (event) => {
+			if (formRef.current && !formRef.current.contains(event.target)) {
+				onClose()
+			}
+		}
+
+		document.addEventListener('mousedown', handleClickOutside)
+		return () => document.removeEventListener('mousedown', handleClickOutside)
+	}, [onClose])
+	const priorities = [priority].concat(
+		['low', 'medium', 'high'].filter((pri) => pri !== priority)
+	)
+	const dropdownProjects = [chosenProject].concat(
+		projects.filter((prj) => prj.name !== chosenProject).map((prj) => prj.name)
+	)
+	const handleAddTask = (e) => {
+		e.preventDefault()
+
+		// console.log("what");
+
+		if (taskName.value === '') {
+			dispatch(setError('Please enter a task name', 2))
+			return
+		}
+
+		// console.log("the");
+		const taskToUpdate = {
+			name: taskName.value,
+			priority: priority,
+			project: projects.filter((prj) => prj.name === chosenProject)[0].id,
+			id: task.id,
+		}
+		try {
+			dispatch(updateTask(taskToUpdate))
+			onClose()
+		} catch {
+			dispatch(setError('Something goes wrong', 5))
+		}
+	}
+
+	return (
+		<div ref={formRef}>
+			<h1>Edit Task</h1>
+			<button onClick={onClose}>X</button>
+			<div className='task-name'>
+				<label>Task Name</label>
+				<br />
+				<input {...taskName} />
+			</div>
+			<div className='task-priority'>
+				<label>Task Priority</label>
+				<br />
+				<Dropdown options={priorities} onSelect={setPriority} />
+			</div>
+			<div className='task-project'>
+				<label>Project</label>
+				<br />
+				<Dropdown options={dropdownProjects} onSelect={setChosenProject} />
+			</div>
+			<button onClick={onClose}>Cancel</button>
+			<button onClick={handleAddTask}>Edit Task</button>
+		</div>
+	)
 }
 
 export default EditTaskForm
+
