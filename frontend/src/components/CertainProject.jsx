6f4ea9470eb7756fb673aba747f8c82aad2a0a0e
peogway <duchungbeo2004@gmail.com>
2025-02-09 14:08:10 +0200

    CertainProject component initial version

---
+++ frontend/src/components/CertainProject.jsx
@@ -0,0 +1,8 @@
+import { useState, useEffect, useRef } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
+const CertainProject = ({ project, categories, onClose }) => {
+	return <div></div>
+}
+
+export default CertainProject
