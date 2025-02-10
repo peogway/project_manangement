8097d6c911453ca00d535f69b7cf092ac8e04295
peogway <duchungbeo2004@gmail.com>
2025-02-10 16:11:57 +0200

    add ref to access specific value

--- frontend/src/components/DropDown.jsx
+++ frontend/src/components/DropDown.jsx
@@ -1,36 +1,39 @@
-import React, { useState } from 'react'
+import React, { useState, forwardRef } from 'react'
 
-const Dropdown = ({ options, onSelect, description, value }) => {
-	const [selectedOption, setSelectedOption] = useState(
-		description !== undefined ? '' : options[0]
-	)
+const Dropdown = forwardRef(
+	({ options, onSelect, description, value }, ref) => {
+		const [selectedOption, setSelectedOption] = useState(
+			description !== undefined ? '' : options[0]
+		)
 
-	const handleSelectOption = (e) => {
-		if (!value) {
-			setSelectedOption(e.target.value)
+		const handleSelectOption = (e) => {
+			if (!value) {
+				setSelectedOption(e.target.value)
+			}
+			onSelect(e.target.value) // Pass selected option back to parent
 		}
-		onSelect(e.target.value) // Pass selected option back to parent
-	}
 
-	return (
-		<select
-			className={`dropdown ${selectedOption === '' ? 'default' : ''}`}
-			value={selectedOption}
-			onChange={handleSelectOption}
-		>
-			{description && (
-				<option value='' disabled>
-					{description}
-				</option>
-			)}
-			{options.map((option) => (
-				<option key={option} value={option}>
-					{option}
-				</option>
-			))}
-		</select>
-	)
-}
+		return (
+			<select
+				ref={ref}
+				className={`dropdown ${selectedOption === '' ? 'default' : ''}`}
+				value={selectedOption}
+				onChange={handleSelectOption}
+			>
+				{description && (
+					<option value='' disabled>
+						{description}
+					</option>
+				)}
+				{options.map((option) => (
+					<option key={option} value={option}>
+						{option}
+					</option>
+				))}
+			</select>
+		)
+	}
+)
 
 export default Dropdown
 
