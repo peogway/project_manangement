919071769d14d674f9e1d6c1b66fb8e72f92c7fb
peogway <duchungbeo2004@gmail.com>
2025-02-19 00:32:24 +0200

    update progress bar component

--- frontend/src/components/ProgressBar.jsx
+++ frontend/src/components/ProgressBar.jsx
@@ -1,25 +1,20 @@
 import { useState } from 'react'
 
-const ProgressBar = () => {
-	const [progress, setProgress] = useState(50) // Set initial progress
+const ProgressBar = ({ progress }) => {
+	console.log(progress)
 
 	return (
 		<div className='w-full max-w-md p-4'>
 			<div className='w-full bg-gray-200 rounded-full h-4'>
 				<div
-					className='bg-blue-500 h-4 rounded-full transition-all'
-					style={{ width: `${progress}%` }}
-				></div>
+					className='bg-green-500 h-4 rounded-full transition-all'
+					style={{ width: `${progress * 100}%` }}
+				>
+					{(progress * 100).toFixed(0)}%
+				</div>
 			</div>
-			<button
-				className='mt-4 px-4 py-2 bg-blue-600 text-white rounded'
-				onClick={() => setProgress((prev) => (prev >= 100 ? 0 : prev + 10))}
-			>
-				Increase Progress
-			</button>
 		</div>
 	)
 }
 
 export default ProgressBar
-
