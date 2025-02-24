5d208fc5caf25a0700fce5dc786906ca4bf877de
peogway <duchungbeo2004@gmail.com>
2025-02-24 19:00:36 +0200

    move EditCategoryForm with style to a separate component

--- frontend/src/components/Category.jsx
+++ frontend/src/components/Category.jsx
@@ -1,91 +1,10 @@
 import { useState, useRef, useEffect } from 'react'
-import { updateCategory, deleteCategory } from '../reducers/categoryReducer'
+import { deleteCategory } from '../reducers/categoryReducer'
 import { useDispatch } from 'react-redux'
-import { useField } from '../hooks/hook'
-import { setError, setNotification } from '../reducers/notiReducer'
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz'
 import EditIcon from '@mui/icons-material/Edit'
 import DeleteIcon from '@mui/icons-material/Delete'
-
-const EditCategoryForm = ({ onClose, name, categories, id }) => {
-	const formRef = useRef(null)
-	const { remove: rmTask, ...categoryName } = useField('text', name)
-	const overlayRef = useRef(null)
-	const dispatch = useDispatch()
-	const handleClickOutside = (event) => {
-		if (formRef.current && !formRef.current.contains(event.target)) {
-			onClose()
-		}
-	}
-
-	const handleEditCategory = (e) => {
-		e.preventDefault()
-		if (categoryName.value === '') {
-			dispatch(setError('Please enter a category name', 2))
-
-			return
-		}
-
-		if (categories.some((category) => category.name === categoryName.value)) {
-			dispatch(setError('Categories must be unique', 2))
-			onClose()
-			return
-		}
-		const category = {
-			name: categoryName.value,
-			id: id,
-		}
-
-		try {
-			dispatch(updateCategory(category))
-
-			onClose()
-		} catch {
-			dispatch(setError('Something goes wrong', 5))
-		}
-	}
-	return (
-		<div>
-			<div
-				ref={overlayRef}
-				onClick={handleClickOutside}
-				style={{
-					position: 'fixed',
-					top: 0,
-					left: 0,
-					width: '100%',
-					height: '100%',
-					backgroundColor: 'rgba(0, 0, 0, 0.5)',
-					zIndex: 999,
-					pointerEvents: 'auto',
-				}}
-			/>
-			<div
-				ref={formRef}
-				style={{
-					position: 'absolute',
-					top: '50%',
-					left: '50%',
-					transform: 'translate(-50%, -50%)',
-					background: 'white',
-					padding: 20,
-					zIndex: 1000,
-				}}
-			>
-				<h1>Edit Category</h1>
-				<button onClick={onClose}>X</button>
-				<div className='category-name'>
-					<label>Cateory Name</label>
-					<br />
-					<input {...categoryName} />
-				</div>
-
-				<button onClick={onClose}>Cancel</button>
-				<button onClick={handleEditCategory}>Edit Category</button>
-			</div>
-		</div>
-	)
-}
+import EditCategoryForm from './EditCategoryForm'
 
 const Category = (props) => {
 	const featureRef = useRef(null)
@@ -119,8 +38,8 @@ const Category = (props) => {
 	return (
 		<div>
 			<div className='category-container flex justify-between items-center'>
-				<div className='Category-name-project ml-4 p-3 text-xl'>
-					<h2 className='category-name font-bold'>{props.name}</h2>
+				<div className='Category-name-project ml-4 p-3'>
+					<h2 className='category-name font-bold text-xl'>{props.name}</h2>
 					<p className='category-project text-gray-400 ml-1'>
 						{props.projects.length} Projects
 					</p>
