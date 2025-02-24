9f4bea1d61eab1442c6d9f2e2f7e2ccd1cb86511
peogway <duchungbeo2004@gmail.com>
2025-02-24 21:43:37 +0200

    style progress bar again

--- frontend/src/components/ProgressBar.jsx
+++ frontend/src/components/ProgressBar.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
 
-const ProgressBar = ({ progress }) => {
+const ProgressBar = ({ progress, color, showPercent }) => {
 	return (
-		<div className='w-full max-w-md p-4'>
-			<div className='w-full bg-gray-200 rounded-full h-4'>
+		<div className='w-[200px] max-w-md '>
+			<div className='w-full bg-gray-200 rounded-full h-1'>
 				<div
-					className='bg-green-500 h-4 rounded-full transition-all'
+					className={`${color} h-1 rounded-full transition-all`}
 					style={{ width: `${progress * 100}%` }}
 				>
-					{(progress * 100).toFixed(0)}%
+					{showPercent && `${(progress * 100).toFixed(0)}%`}
 				</div>
 			</div>
 		</div>
