e9ff57ae8647f58ee0293c8738b39e7151a490a6
peogway <duchungbeo2004@gmail.com>
2025-02-28 13:03:25 +0200

    style notification component, move it to top center of the screen

--- frontend/src/components/Notification.jsx
+++ frontend/src/components/Notification.jsx
@@ -1,12 +1,14 @@
-import { useDispatch } from 'react-redux'
-import { setNotification } from '../reducers/notiReducer'
+// import { useDispatch } from 'react-redux'
+// import { setNotification } from '../reducers/notiReducer'
 
+// eslint-disable-next-line react/prop-types
 const Notification = ({ message, className }) => {
+  
   if (!message) {
     return null
   }
 
-  return <div className={className}>{message}</div>
+  return <div className={`${className} absolute top-4 left-1/2 transform -translate-x-1/2 bg-red-500 text-white px-4 py-2 rounded-xl shadow-lg`}>{message}</div>
 }
 
 export default Notification
