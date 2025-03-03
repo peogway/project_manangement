7ffd9a38944107167586ae61519fb565f274e6fc
peogway <duchungbeo2004@gmail.com>
2025-03-03 02:12:59 +0200

    add padding left

--- frontend/src/components/DropDown.jsx
+++ frontend/src/components/DropDown.jsx
@@ -18,7 +18,7 @@ const Dropdown = forwardRef(
 				ref={ref}
 				className={`dropdown ${
 					selectedOption === '' ? 'default' : ''
-				} text-gray-500 border-1 border-gray-400 rounded-lg w-${width}`}
+				} text-gray-500 border-1 border-gray-400 rounded-lg w-${width} pl-3`}
 				value={selectedOption}
 				onChange={handleSelectOption}
 			>
