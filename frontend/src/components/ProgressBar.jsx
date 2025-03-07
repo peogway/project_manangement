6d3971daf2c647984f0cd31917f66a3964249c8b
peogway <duchungbeo2004@gmail.com>
2025-03-07 16:10:02 +0200

    lighter color

--- frontend/src/components/ProgressBar.jsx
+++ frontend/src/components/ProgressBar.jsx
@@ -5,7 +5,7 @@ const ProgressBar = ({ progress, color, className, height }) => {
 				className={`w-full bg-gray-200 rounded-full ${height ? height : 'h-2'}`}
 			>
 				<div
-					className={`${color ? color : 'bg-orange-600'} ${
+					className={`${color ? color : 'bg-orange-500'} ${
 						height ? height : 'h-2'
 					} rounded-full transition-all`}
 					style={{ width: `${progress}%` }}
