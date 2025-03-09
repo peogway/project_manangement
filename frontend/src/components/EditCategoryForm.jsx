4c3819dc23366a386bfdd5dc6e35e9123287273d
peogway <duchungbeo2004@gmail.com>
2025-03-09 20:26:37 +0200

    refetch projects whenever edit

--- frontend/src/components/EditCategoryForm.jsx
+++ frontend/src/components/EditCategoryForm.jsx
@@ -5,7 +5,7 @@ import { useField } from '../hooks/hook'
 import CloseIcon from '@mui/icons-material/Close'
 import { updateCategory } from '../reducers/categoryReducer'
 
-const EditCategoryForm = ({ onClose, name, categories, id }) => {
+const EditCategoryForm = ({ onClose, name, categories, id, reFetch }) => {
 	const formRef = useRef(null)
 	const { remove: rmTask, ...categoryName } = useField('text', name)
 	const overlayRef = useRef(null)
@@ -36,7 +36,7 @@ const EditCategoryForm = ({ onClose, name, categories, id }) => {
 
 		try {
 			dispatch(updateCategory(category))
-
+			reFetch()
 			onClose()
 		} catch {
 			dispatch(setError('Something goes wrong', 5))
