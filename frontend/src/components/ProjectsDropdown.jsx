fb90dfcc5d5b3abf9ff57a3464b7aedf9d2575e1
peogway <duchungbeo2004@gmail.com>
2025-03-11 18:59:22 +0200

    min-w and max-w nowrap whitespace

--- frontend/src/components/ProjectsDropdown.jsx
+++ frontend/src/components/ProjectsDropdown.jsx
@@ -46,7 +46,7 @@ const ProjectsDropDown = ({
 			ref={dropDownRef}
 			className={` ${
 				openProjectsDropDown ? 'block' : 'hidden'
-			}    overflow-auto bg-white absolute p-3 select-none  user-select-none border border-slate-50 w-auto shadow-md box rounded-lg flex flex-col gap-2 
+			} min-w-[250px] max-w-[600px] whitespace-nowrap bg-white absolute p-3 select-none  user-select-none border border-slate-50 w-auto shadow-md box rounded-lg flex flex-col gap-2 
 			${
 				showAllProject === true
 					? ' top-15 left-15 max-h-[calc(100vh-300px)]'
