edd317387451fe5a7c6cda8b73e9d789331d1e38
peogway <duchungbeo2004@gmail.com>
2025-03-15 07:13:31 +0200

    initial structure

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -5,11 +5,14 @@ import { setAllCategories } from '../reducers/categoryReducer'
 import { setAllProject } from '../reducers/prjReducer'
 import { setAllTasks } from '../reducers/taskReducer'
 import { getIconComponent } from './AllIcons'
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faListCheck } from '@fortawesome/free-solid-svg-icons'
 import { faBarsProgress } from '@fortawesome/free-solid-svg-icons'
 import { faDiagramProject } from '@fortawesome/free-solid-svg-icons'
 import { faLayerGroup } from '@fortawesome/free-solid-svg-icons'
+
+import { BarChart, Bar, ResponsiveContainer } from 'recharts'
 
 import ProjectForm from './ProjectForm'
 import ProgressBar from './ProgressBar'
@@ -34,9 +37,17 @@ const Dashboard = ({ user }) => {
 	const categories = useSelector((state) => state.categories)
 	const tasks = useSelector((state) => state.tasks)
 
+	const data = [
+		{
+			name: 'Page A',
+			uv: 4000,
+			pv: 2400,
+			amt: 2400,
+		},
+	]
 	return (
 		<div className='z-999 flex flex-row h-screen flex-1 overflow-auto left-[60px] max-w-[calc(100vw-60px)]  relative'>
-			<div className='min-h-[100px] left-[20px] right-0 box flex flex-row justify-between items-center z-990 bg-white rounded-2xl absolute  '>
+			<div className='min-h-[110px] left-[20px] right-0 box flex flex-row justify-between items-center z-990 bg-white rounded-2xl absolute  '>
 				<div className='flex flex-row justify-between items-center w-full'>
 					<div className='flex flex-col'>
 						<div className=''>Hello, {user.name}</div>
@@ -50,10 +61,7 @@ const Dashboard = ({ user }) => {
 			</div>
 
 			{/* General stats */}
-			<div
-				className='absolute flex flex-row gap-7 p-3 
-			top-[120px] right-[300px] left-[30px] bg-white rounded-xl box'
-			>
+			<div className='absolute flex flex-row gap-7 p-3 top-[130px] right-[400px] left-[75px] bg-white rounded-xl box'>
 				<div
 					className='bg-orange-500  items-center text-white p-2 flex flex-row flex-1/3 rounded-2xl cursor-pointer select-none'
 					onClick={() => {
@@ -108,8 +116,63 @@ const Dashboard = ({ user }) => {
 				</div>
 			</div>
 
+			{/* Barchart */}
+			<div className='absolute p-5 bg-white flex flex-col right-[400px] left-[75px] top-[250px]  h-[400px] box rounded-xl'>
+				<BarChart width={150} height={40} data={data} barSize={29}>
+					<Bar dataKey='uv' fill='#8884d8' />
+				</BarChart>
+			</div>
+
+			{/* Recent Tasks */}
+			<div className='absolute p-5 bg-white flex flex-col right-[400px] left-[75px] top-[700px]  box rounded-xl pb-10'>
+				<div className='font-bold text-[]'>Recents Task</div>
+				<div className='mt-5 ml-2 p-2 flex flex-col gap-3 items-center select-none'>
+					{[...tasks].reverse().map(
+						(task, index) =>
+							index < 6 && (
+								<div
+									key={task.id}
+									className='rounded-xl bg-slate-100 p-2 flex cursor-pointer gap-2 w-full justify-between'
+									onClick={(e) => {
+										navigate('/tasks', {
+											state: {
+												project: projects.filter(
+													(project) => project.id === task.project.id
+												)[0],
+											},
+										})
+									}}
+								>
+									<div className='flex justify-center items-center'>
+										{getIconComponent(
+											task.icon,
+											'text-orange-500',
+											'text-[15px]',
+											'bg-white',
+											'p-[2px]'
+										)}
+									</div>
+									<p className='font-bold'>{task.name}</p>
+									<div className='flex flex-col'>
+										<p className='text-slate-400'>Created at</p>
+										<p className='text-blue-500'>{task.createAt}</p>
+									</div>
+									<div className='flex flex-col'>
+										<p className='text-slate-400'>In Project</p>
+										<p className='text-blue-500'>{task.project.name}</p>
+									</div>
+									<div className='flex flex-col'>
+										<p className='text-slate-400'>Status</p>
+										<p className='text-blue-500'>{task.status}</p>
+									</div>
+								</div>
+							)
+					)}
+				</div>
+			</div>
+
 			{/* Overall Progress */}
-			<div className='absolute right-0 top-[120px] bg-white w-[270px]  h-[200px] box rounded-2xl flex items-center justify-center'>
+			<div className='absolute right-0 top-[130px] bg-white w-[320px]  h-[200px] box rounded-2xl flex items-center justify-center'>
 				<div className='flex flex-col justify-between items-center'>
 					<div className='font-bold '>Overall Progress</div>
 					<div className='w-27 h-27 rounded-full bg-orange-500 flex justify-center items-center mt-4'>
@@ -131,7 +194,7 @@ const Dashboard = ({ user }) => {
 			</div>
 
 			{/* Latest Projects */}
-			<div className='pb-10 absolute right-0 top-[340px] w-[270px] h-auto min-h-[100px] rounded-2xl flex flex-col box p-1'>
+			<div className='pb-10 absolute right-0 top-[350px] w-[320px] h-auto min-h-[100px] rounded-2xl flex flex-col box p-1'>
 				<div className='flex items-center justify-between p-4 w-full '>
 					<div className='font-bold'>Latest Projects</div>
 					<div
@@ -142,59 +205,62 @@ const Dashboard = ({ user }) => {
 					</div>
 				</div>
 				<div className='flex flex-col gap-7 p-2 '>
-					{[...projects].reverse().map((project) => (
-						<div
-							key={project.id}
-							className='rounded-xl bg-slate-100 p-2 flex flex-col w-full'
-							onClick={(e) => {
-								navigate('/tasks', {
-									state: { project: project },
-								})
-							}}
-						>
-							<div className='flex p-1 gap-1'>
-								<div className='flex justify-center items-center'>
-									{getIconComponent(
-										project.icon,
-										'text-white',
-										'text-[15px]',
-										'bg-orange-500',
-										'p-[2px]'
-									)}
-								</div>
-								<div className='text-slate-500'>{project.name}</div>
-							</div>
+					{[...projects].reverse().map(
+						(project, index) =>
+							index < 4 && (
+								<div
+									key={project.id}
+									className='rounded-xl bg-slate-100 p-2 flex flex-col w-full'
+									onClick={(e) => {
+										navigate('/tasks', {
+											state: { project: project },
+										})
+									}}
+								>
+									<div className='flex p-1 gap-1'>
+										<div className='flex justify-center items-center'>
+											{getIconComponent(
+												project.icon,
+												'text-white',
+												'text-[15px]',
+												'bg-orange-500',
+												'p-[2px]'
+											)}
+										</div>
+										<div className='text-slate-500'>{project.name}</div>
+									</div>
 
-							<div className='flex w-full justify-between items-center text-slate-500 mt-4'>
-								<div className='flex gap-1 text-slate-500 items-center justify-center'>
-									<FontAwesomeIcon icon={faBarsProgress} />
+									<div className='flex w-full justify-between items-center text-slate-500 mt-4'>
+										<div className='flex gap-1 text-slate-500 items-center justify-center'>
+											<FontAwesomeIcon icon={faBarsProgress} />
 
-									<p className='self-start'>Progress</p>
-								</div>
-								<div className=''>
-									{project.tasks.filter((task) => task.completed).length}/
-									{project.tasks.length}
-								</div>
-							</div>
+											<p className='self-start'>Progress</p>
+										</div>
+										<div className=''>
+											{project.tasks.filter((task) => task.completed).length}/
+											{project.tasks.length}
+										</div>
+									</div>
 
-							<div className='mt-2 mb-2'>
-								<ProgressBar
-									progress={
-										project.tasks.length === 0
-											? 0
-											: Math.floor(
-													(project.tasks.filter((task) => task.completed)
-														.length /
-														project.tasks.length) *
-														100
-											  )
-									}
-									height='h-1'
-									bgColor='bg-gray-300'
-								/>
-							</div>
-						</div>
-					))}
+									<div className='mt-2 mb-2'>
+										<ProgressBar
+											progress={
+												project.tasks.length === 0
+													? 0
+													: Math.floor(
+															(project.tasks.filter((task) => task.completed)
+																.length /
+																project.tasks.length) *
+																100
+													  )
+											}
+											height='h-1'
+											bgColor='bg-gray-300'
+										/>
+									</div>
+								</div>
+							)
+					)}
 				</div>
 			</div>
 
