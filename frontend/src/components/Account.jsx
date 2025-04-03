531bfbab2b72d40f61f5394c18569278694977a8
peogway <duchungbeo2004@gmail.com>
2025-04-04 00:18:25 +0300

    initial account component

---
+++ frontend/src/components/Account.jsx
@@ -0,0 +1,17 @@
+import { useState, useEffect, useRef } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { setUserFn } from '../reducers/userReducer'
+import Heading from './Heading'
+
+const Account = () => {
+	const dispatch = useDispatch()
+	return (
+		<div className='z-999 flex flex-row h-screen flex-1 overflow-auto left-[60px] max-w-[calc(100vw-60px)]  relative'>
+			<Heading name='Account' />
+		</div>
+	)
+}
+
+export default Account
+
