1a55568a29bb0896ce91f3b1473ab6789c0efa37
peogway <duchunbeo2004@gmail.com>
2025-04-06 07:54:08 +0300

    prettier format

--- frontend/src/reducers/userReducer.js
+++ frontend/src/reducers/userReducer.js
@@ -1,28 +1,28 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit'
 
 const userSlice = createSlice({
-    name: "user",
-    initialState: null,
-    reducers: {
-        setUser(state, action) {
-            return action.payload;
-        },
-        removeUser(state, action) {
-            return null;
-        },
-    },
-});
+	name: 'user',
+	initialState: null,
+	reducers: {
+		setUser(state, action) {
+			return action.payload
+		},
+		removeUser(state, action) {
+			return null
+		},
+	},
+})
 
-export const { setUser, removeUser } = userSlice.actions;
+export const { setUser, removeUser } = userSlice.actions
 
 export const setUserFn = (user) => {
-    return (dispatch) => {
-        dispatch(setUser(user));
-    };
-};
+	return (dispatch) => {
+		dispatch(setUser(user))
+	}
+}
 
 export const rmUserFn = () => {
-    return (dispatch) => dispatch(removeUser());
-};
+	return (dispatch) => dispatch(removeUser())
+}
 
-export default userSlice.reducer;
+export default userSlice.reducer
