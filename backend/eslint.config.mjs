adc073a0c658d6aa8f9084713c92de9a5c07fe4a
peogway <duchunbeo2004@gmail.com>
2025-04-06 11:18:52 +0300

    default end of line to crlf

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -3,50 +3,32 @@ import stylisticJs from "@stylistic/eslint-plugin-js";
 import js from "@eslint/js";
 
 export default [
-  js.configs.recommended,
-  {
-    files: ["**/*.js"],
-    languageOptions: {
-      sourceType: "commonjs",
-      globals: {
-        ...globals.node,
-      },
-      ecmaVersion: "latest",
-    },
-    plugins: {
-      "@stylistic/js": stylisticJs,
-    },
-    rules: {
-      "@stylistic/js/indent": [
-        "error",
-        4,
-      ],
-      "@stylistic/js/linebreak-style": [
-        "error",
-        "unix",
-      ],
-      "@stylistic/js/quotes": [
-        "error",
-        "double",
-      ],
-      "@stylistic/js/semi": [
-        "error",
-        "always",
-      ],
-      "eqeqeq": "error",
-      "no-trailing-spaces": "error",
-      "object-curly-spacing": [
-        "error",
-        "always",
-      ],
-      "arrow-spacing": [
-        "error",
-        { "before": true, "after": true },
-      ],
-      "no-console": "off",
-    },
-  },
-  {
-    ignores: ["dist/**", "build/**"],
-  },
-];
+	js.configs.recommended,
+	{
+		files: ['**/*.js'],
+		languageOptions: {
+			sourceType: 'commonjs',
+			globals: {
+				...globals.node,
+			},
+			ecmaVersion: 'latest',
+		},
+		plugins: {
+			'@stylistic/js': stylisticJs,
+		},
+		rules: {
+			'@stylistic/js/indent': ['error', 4],
+			'@stylistic/js/linebreak-style': ['error', 'windows'],
+			'@stylistic/js/quotes': ['error', 'double'],
+			'@stylistic/js/semi': ['error', 'always'],
+			eqeqeq: 'error',
+			'no-trailing-spaces': 'error',
+			'object-curly-spacing': ['error', 'always'],
+			'arrow-spacing': ['error', { before: true, after: true }],
+			'no-console': 'off',
+		},
+	},
+	{
+		ignores: ['dist/**', 'build/**'],
+	},
+]
