175ababb7c27b37665db57f7621eff6df8ef05f0
peogway <duchungbeo2004@gmail.com>
2025-04-15 17:05:39 +0300

    display icons

--- frontend/src/components/Notification.jsx
+++ frontend/src/components/Notification.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import ErrorIcon from '@mui/icons-material/Error'
+import TaskAltIcon from '@mui/icons-material/TaskAlt'
 
 const Notification = ({ message, className }) => {
 	if (!message) {
@@ -7,7 +9,7 @@ const Notification = ({ message, className }) => {
 	}
 
 	const baseStyles =
-		'z-10000 fixed top-0 left-1/2 transform -translate-x-1/2 px-6 py-3 rounded-xl shadow-lg transition-transform duration-500 ease-in-out'
+		'z-10000 fixed flex items-center gap-2 text-xl top-0 left-1/2 transform -translate-x-1/2 px-6 py-3 rounded-xl shadow-lg transition-transform duration-500 ease-in-out'
 	const colorStyles =
 		className === 'error'
 			? 'bg-red-500 text-white'
@@ -21,6 +23,9 @@ const Notification = ({ message, className }) => {
 				message ? 'translate-y-12' : '-translate-y-full'
 			}`}
 		>
+			<div className='text-white'>
+				{className === 'error' ? <ErrorIcon /> : <TaskAltIcon />}
+			</div>
 			{message}
 		</div>
 	)
