ace9b02ca5272ca97c4b92334c3dda5612cedc9f
duchungbeo2004@gmail.com <duchungbeo2004@gmail.com>
2025-07-20 23:58:47 +0300

    feat: integrate language dropdown

--- frontend/src/components/Heading.jsx
+++ frontend/src/components/Heading.jsx
@@ -2,15 +2,31 @@ import { useState, useEffect, useRef } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import Avatar from './Avatar'
+import { useTranslation } from 'react-i18next'
+import LanguageDropDown, { getCard } from './LanguageDropDown'
+
 const Heading = ({ name, user }) => {
+	const { t, i18n } = useTranslation()
+	const [chosenCard, setChosenCard] = useState(getCard)
+	const [openLanguageDropDown, setOpenLanguageDropDown] = useState(false)
 	return (
 		/* Heading */
-		<div className='min-h-[110px] left-[20px] right-0 box flex flex-row justify-between items-center z-990 bg-white rounded-2xl absolute  '>
+		<div className='min-h-[110px] left-[20px] right-0 box flex flex-row justify-between z-990 bg-white rounded-2xl absolute  '>
 			<div className='flex w-full justify-end select-none relative'>
 				<div className='absolute top-[50%] translate-y-[-50%] left-15'>
 					<h1 className='text-3xl text-slate-800 font-bold'>
 						My <span className='text-orange-600'>{name}</span>
 					</h1>
+				</div>
+
+				{/* Display language options */}
+				<div className='mt-10'>
+					<LanguageDropDown
+						openLanguageDropDown={openLanguageDropDown}
+						setOpenLanguageDropDown={setOpenLanguageDropDown}
+						setChosenCard={setChosenCard}
+						chosenCard={chosenCard}
+					/>
 				</div>
 
 				{/* Avartar */}
@@ -23,4 +39,3 @@ const Heading = ({ name, user }) => {
 }
 
 export default Heading
-
