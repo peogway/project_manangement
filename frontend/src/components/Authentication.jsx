b13e851af04bebcbd55e3c331f5c8e94dbd0ddb2
duchungbeo2004@gmail.com <duchungbeo2004@gmail.com>
2025-07-22 06:42:49 +0300

    fix: update language dropdown position to fixed for better visibility

--- frontend/src/components/Authentication.jsx
+++ frontend/src/components/Authentication.jsx
@@ -304,7 +304,7 @@ const Authentication = () => {
 				</div>
 			</div>
 			{/* Display language options */}
-			<div className='absolute top-5 left-0 '>
+			<div className='fixed top-5 left-[50%] '>
 				<LanguageDropDown
 					openLanguageDropDown={openLanguageDropDown}
 					setOpenLanguageDropDown={setOpenLanguageDropDown}
